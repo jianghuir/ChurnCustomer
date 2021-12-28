2d5b6372607055aa884509edcac1e078fc3880a9
Jian Gao <linuxkid@gmail.com>
2021-12-27 20:02:20 -0500

    able to show shap force_plot! updated comments in main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request
-#import matplotlib.pyplot as plt
-#import matplotlib
 import joblib
 import pandas as pd
 import numpy as np
 import shap
+from PIL import Image
  
 
 # create Flask App
@@ -12,20 +11,21 @@ app = Flask(__name__)  # two underscores here
 
 model = joblib.load("src/churn_model.pkl")
 
-# connect POST API call --> predict() function
 
-@app.route("/", methods=["GET","POST"])       
-#"app" here is the Flask app created above
 #we will create a local server at http://localhost:5000
-#.route('/churn') means when app meets http://localhost:5000/churn
-# this whole line means when app meets http://localhost:5000/churn, you should be able to post some data to the api
-# the data posted is in JSON format, which looks like a python dictionary 
+@app.route("/", methods=["GET","POST"])       
 
 def predict():
     
-    results = ""
+    pred = ""
+    churn_probability = ""
+    shap_html = Image.open("static/Blank.jpeg")
+    note1 = ""
+    note2 = ""
+    note3 = ""
     
     if request.method == "POST":
+        # get customer info from index.html
         features = {}
         features["Age"] = float(request.form["Age"])
         features['Dependent#'] = float(request.form['Dependent#'])
@@ -40,29 +40,36 @@ def predict():
         features['TotalTransCt'] = float(request.form['TotalTransCt'])
         features['TransCtQ4/Q1'] = float(request.form['TransCtQ4/Q1'])
         
+        # create three new features
         features['RevolvBal/CreditLimit'] = np.round(features['RevolvBal']/features['CreditLimit'], 5)
         features['TotalTransAmt/CreditLimit/12'] = np.round(features['TotalTransAmt']/features['CreditLimit']/12, 5)
         features['AverageCost'] = np.round(features['TotalTransAmt']/features['TotalTransCt'], 3)
   
+        # use model to predict
         df = pd.DataFrame(features, index=[0])
         churn_class = model.predict(df)
         churn_probability = np.round(model.predict_proba(df)[0][1], 3)
                
+        if churn_class[0] == 1:
+            pred = "Yes"
+        else:
+            pred = "No"
         
-        
-        # get shap image. the third line can't run in API, but it works well in Jupyter Notebook
+        # get shap image and print on the webpage
         shap_explainer = shap.TreeExplainer(model)
         shap_v_customer = shap_explainer.shap_values(df.iloc[0])
         p=shap.force_plot(shap_explainer.expected_value[1],shap_v_customer[1],df.iloc[0])
-        #plt.savefig('static/image.png', bbox_inches='tight', dpi=100)
+        shap_html = f"<head>{shap.getjs()}</head><body>{p.html()}</body>"
         
-        results = {'Churn prediction (1-Yes, 0-No)':churn_class[0], 'Churn probability':churn_probability}
-        
-    return render_template("index.html", result=results)
+        note1 = "f(x) value (in bold font) is the predicted churning probability of the customer, ranging from 0.0-1.0. For example, with f(x) 0.90, the customer is predicted to have a churning probability of 90%"
+        note2 = "Red blocks are factors leading to a higher churning probability. Blue blocks are factors leading to a lower churning probability."
+        note3 = "A bigger block poses a higher influence on churning probability"
+
+    return render_template("index.html", result1=pred, result2=churn_probability, plot_cust=shap_html, imageNote1=note1, imageNote2=note2, imageNote3=note3)
 
 
         
         
-# Load model and col_name
+# Load model
 if __name__ == "__main__":
     app.run(debug=True, host='127.0.0.1', port=5000)
